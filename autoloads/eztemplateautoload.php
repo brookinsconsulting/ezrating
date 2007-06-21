c02a32b3345b7d9d2c2846eccee2fe2236eb4751
Kristof Coomans <kristof.coomans@telenet.be>
2007-06-21 14:56:14 +0000

    - Initial ezrating version

---
+++ autoloads/eztemplateautoload.php
@@ -0,0 +1,8 @@
+<?php
+
+$eZTemplateOperatorArray = array();
+$eZTemplateOperatorArray[] = array( 'script' => 'extension/ezrating/autoloads/ezratingoperator.php',
+                                    'class' => 'eZRatingOperator',
+                                    'operator_names' => array( 'ezrating_summary' ) );
+
+?>
