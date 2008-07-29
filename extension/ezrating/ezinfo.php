180c7015b3ee6531172910c1c58084fb23030cad
Kristof Coomans <kristof.coomans@telenet.be>
2008-07-29 10:29:08 +0000

    - Implemented: ezinfo file

---
+++ extension/ezrating/ezinfo.php
@@ -0,0 +1,16 @@
+<?php
+
+class ezratingInfo
+{
+    function info()
+    {
+        return array(
+            'Name' => '<a href="http://projects.ez.no/ezrating">eZ Rating</a>',
+            'Version' => "1.x",
+            'Copyright' => "Copyright (C) 2007-2008 Kristof Coomans",
+            'License' => "GNU General Public License v2.0"
+        );
+    }
+}
+
+?>
